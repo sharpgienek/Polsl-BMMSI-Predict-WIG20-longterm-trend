f5e9bb6cd2a650431a72f74398699eb641a03608
Przemyslaw Szczotok <longer-1990@tlen.pl>
2012-09-27 11:30:15 +0200

    Dodano komentarz dla klasy PredictionResult.cs

--- DTO/PredictionResult.cs
+++ DTO/PredictionResult.cs
@@ -5,13 +5,34 @@ using System.Text;
 
 namespace DTO
 {
+    /// <summary>
+    /// Klasa służaća do przechowywania wyników przewidywania oraz paramatrów sieci użytej do predykcji.
+    /// </summary>
     public class PredictionResult
     {
+        /// <summary>
+        /// Lista faktycznych wyników - trendy wraz z ich prawdopodobieństwem.
+        /// </summary>
         public List<TrendDirectionWithPropability> Trends { set; get; }
+        /// <summary>
+        /// Liczba okresów - parametr sieci.
+        /// </summary>
         public int PeriodNo { set; get; }
+        /// <summary>
+        /// Liczba wzorców - parametr sieci.
+        /// </summary>
         public int PatternsNo { set; get; }
+        /// <summary>
+        /// Liczba neuronów w warstwie ukrytej - parametr sieci.
+        /// </summary>
         public int NeuronNo { set; get; }
+        /// <summary>
+        /// Liczba epok - parametr sieci.
+        /// </summary>
         public int EpochsNo { set; get; }
+        /// <summary>
+        /// Bład średniokwadratowy sieci.
+        /// </summary>
         public double MSE { set; get; }
     }
 }
